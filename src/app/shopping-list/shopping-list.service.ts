e03e0c66443bf81bf446ea6bc209fb0461226360
Gaurav Sali <gauravps18@gmail.com>
2019-12-02 18:36:13 +0530

    Services - Adding ingredients to recipe list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
-    
+    updatedIngredients = new EventEmitter<Ingredient[]>();
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 10),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    getIngredients() {
+        // slice() return the new copy of an array
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.updatedIngredients.emit(this.ingredients.slice());
+    }
 }
