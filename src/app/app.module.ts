619bbc059348bd44e60e93418a5b602978136df3
Gaurav Sali <gauravps18@gmail.com>
2019-12-17 19:24:59 +0530

    Lazy Loading: Recipes, Shopping-List and Auth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,8 @@ import { HeaderComponent }  from './header/header.component';
 
 // App Modules
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -24,11 +21,8 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
-    CoreModule,
-    AuthModule
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
